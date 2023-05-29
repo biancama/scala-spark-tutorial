2f56ce4d3f27dbb81dddfec008705184a57e0277
Massimo <massimo.biancalani@gmail.com>
2023-05-29 18:42:06 +0200

    Rdd set operations: same host problem

--- src/main/scala/com/sparkTutorial/rdd/nasaApacheWebLogs/SameHostsProblem.scala
+++ src/main/scala/com/sparkTutorial/rdd/nasaApacheWebLogs/SameHostsProblem.scala
@@ -1,7 +1,15 @@
 package com.sparkTutorial.rdd.nasaApacheWebLogs
 
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
 object SameHostsProblem {
 
+  private def notHeader(entry: String): Boolean = !(entry.startsWith("host") && entry.contains("bytes"))
+
+  private def extractHost(rdd: RDD[String]): RDD[String] =
+    rdd.map(line => line.split("\\t")(0))
+      .filter(entry => notHeader(entry))
   def main(args: Array[String]) {
 
     /* "in/nasa_19950701.tsv" file contains 10000 log lines from one of NASA's apache server for July 1st, 1995.
@@ -19,5 +27,14 @@ object SameHostsProblem {
 
        Make sure the head lines are removed in the resulting RDD.
      */
+    val conf = new SparkConf().setAppName("Same host").setMaster("local[4]")
+    val context = new SparkContext(conf)
+
+    val firstOfJuly = context.textFile("in/nasa_19950701.tsv")
+    val firstOfAugust = context.textFile("in/nasa_19950801.tsv")
+
+    extractHost(firstOfJuly)
+      .intersection(extractHost(firstOfAugust))
+      .saveAsTextFile("out/nasa_logs_same_hosts.csv")
   }
 }
